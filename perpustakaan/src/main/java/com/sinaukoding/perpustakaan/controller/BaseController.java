857ce28876e86e997d62507aa7d3d7a7be0486ad
bobbyhans <84373275+bobbyhans@users.noreply.github.com>
2021-10-27 15:44:13 +0700

    Delete perpustakaan directory

--- perpustakaan/src/main/java/com/sinaukoding/perpustakaan/controller/BaseController.java
+++
@@ -1,7 +0,0 @@
-package com.sinaukoding.perpustakaan.controller;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-
-@PreAuthorize("permitAll()")
-public abstract class BaseController {
-}
